9436ac3e86c55396457eb20e2dac55032388a21b
Tyler Barabas <tylerbarabas@gmail.com>
2020-06-30 19:00:44 -0500

    create a new navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/navbar'
 
 const App = () => {
   return (
     <div>
-      Hello world
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/navbar/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar has-background-primary" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://acme.codes">
+          <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28" />
+        </a>
+
+        <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
