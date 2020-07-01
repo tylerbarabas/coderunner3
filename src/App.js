f5d9209be1442debc72b3ca0e4f73b2ffddb4dcc
Tyler Barabas <tylerbarabas@gmail.com>
2020-07-01 11:58:26 -0500

    basic page layout step 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/navbar'
 import QrCodeProvider from './contexts/qrcode'
+import Wrapper from './views/wrapper'
 
 const App = () => {
   return (
     <QrCodeProvider>
       <Navbar />
+      <Wrapper />
     </QrCodeProvider>
   )
 }
