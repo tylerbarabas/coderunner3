da4133313336917d85842ad6fe978a4d6f9de8a3
Tyler Barabas <tylerbarabas@gmail.com>
2020-06-30 19:57:23 -0500

    rework the top bar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,18 +1,30 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   return (
-    <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://acme.codes">
-          <img src="/logo.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28" />
-        </a>
-
-        <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
+    <nav className="navbar is-primary px-6" role="navigation" aria-label="main navigation">
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item" href="https://acme.codes">
+            <FontAwesomeIcon icon={faHome} size='2x' />
+          </a>
+          <div className="columns is-vcentered ml-6 pl-6">
+            <div className="column is-size-4 ml-6 pl-6">
+              For advanced features, <a
+                  href="http://acme.codes/contact"
+                  className="has-text-white"
+                  style={{textDecoration: 'underline'}}
+                >contact us</a>
+            </div>
+          </div>
+        </div>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <img src="/logo.png" alt="Coderunner | Animated QR Code Generator" width="112" height="28" />
+          </div>
+        </div>
       </div>
     </nav>
   )
