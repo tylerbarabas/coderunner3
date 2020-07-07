3923a9df8b04f392143864059cc47d1b3d839080
Tyler Barabas <tylerbarabas@gmail.com>
2020-07-06 19:14:23 -0500

    use scss file

--- src/components/preview/index.js
+++ src/components/preview/index.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './index.scss'
 
-const MIN_WIDTH = '70%'
 export default class Preview extends React.Component {
   getPreview(src){
     if (src.indexOf('/frames/1') !== -1) {
       return (
         <img
           src={src}
-          style={{
-            minWidth: MIN_WIDTH,
-          }}
+          className="preview"
         />
       )
     }
@@ -21,9 +19,6 @@ export default class Preview extends React.Component {
         playsInline
         loop
         muted
-        style={{
-          minWidth: MIN_WIDTH,
-        }}
       />
     )
   }
@@ -32,8 +27,8 @@ export default class Preview extends React.Component {
       src,
     } = this.props
     return (
-      <div className="columns is-vcentered is-fullheight">
-        <div className="column  has-text-centered">
+      <div className="columns is-vcentered is-fullheight preview">
+        <div className="column  has-text-centered preview">
           {this.getPreview(src)}
         </div>
       </div> 
