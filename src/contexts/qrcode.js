078e7c1c1a568f9bfac5b5f51a95726bbe546877
Tyler Barabas <tylerbarabas@gmail.com>
2020-07-08 15:42:50 -0500

    back to work!

--- src/contexts/qrcode.js
+++ src/contexts/qrcode.js
@@ -18,7 +18,20 @@ const DEFAULT = {
 const QrCodeProvider = props => {
   const [ qrCode, dispatch ] = useReducer(qrCodeReducer, DEFAULT)
 
-  const _updateCallStatus = (s, id) => {
+  
+  const apicall = async args => {
+    updateCallStatus('requesting')
+    const r = await Service.createStaticCode(args)
+    let newid = null
+    if (r.ok) {
+      newid = await r.json()
+      updateCallStatus('success', newid)
+    } else {
+      updateCallStatus('fail')
+    }
+  }
+
+  const updateCallStatus = (s, id) => {
     let obj = {}
     switch(s){
       case 'success':
@@ -42,11 +55,16 @@ const QrCodeProvider = props => {
     dispatch(obj)
   }
 
+  let throttle = null
   const updateStr = str => {
-    dispatch({
-      type: 'UPDATE_STRING',
-      str,
-    })
+    throttle = setTimeout(()=>{
+      console.log('update string')
+      dispatch({
+        type: 'UPDATE_STRING',
+        str,
+      })
+    },1000)
+    if (throttle) clearTimeout(throttle)
   }
 
   const qrCodeMethods = {
@@ -54,17 +72,6 @@ const QrCodeProvider = props => {
   }
 
   useEffect(()=>{
-    const apicall = async args => {
-      _updateCallStatus('requesting')
-      const r = await Service.createStaticCode(args)
-      let newid = null
-      if (r.ok) {
-        newid = await r.json()
-        _updateCallStatus('success', newid)
-      } else {
-        _updateCallStatus('fail')
-      }
-    }
     if (qrCode.str !== '') {
       const params = {
         msg: qrCode.str,
