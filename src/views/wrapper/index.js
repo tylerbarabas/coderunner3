eabb26451b23700bcee16d0081e517fc909a47fa
Tyler Barabas <tylerbarabas@gmail.com>
2020-07-08 16:10:42 -0500

    getPreviewUri

--- src/views/wrapper/index.js
+++ src/views/wrapper/index.js
@@ -3,12 +3,13 @@ import Preview from '../../components/preview'
 
 class Wrapper extends React.Component {
   render() {
+    const getPreviewUri = id => `https://api.acme.codes/orders/${id}/frames/1`
     return (
       <div className="section is-fullheight">
         <div className="columns is-fullheight">
           <div className="column is-half has-background-white">
             <Preview
-              src="https://api.acme.codes/orders/1593144341_QZB842DI/frames/1"
+              src={getPreviewUri('1593144341_QZB842DI')}
             />
           </div>
           <div className="column is-half has-text-white has-text-centered">
