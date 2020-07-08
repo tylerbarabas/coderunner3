dca3ad034879ddf67601710411be141d6f112c4a
Tyler Barabas <tylerbarabas@gmail.com>
2020-07-08 17:09:44 -0500

    defaults

--- src/contexts/responsive.js
+++ src/contexts/responsive.js
@@ -3,3 +3,11 @@ import React {
 } from 'react'
 
 import responsiveReducer from '../reducers/responsive'
+
+const DEFAULT = {
+  isMobile: false,
+  isTouch: false,
+  isDesktop: false,
+  isWideScreen: false,
+  isFullHD: false,
+}
