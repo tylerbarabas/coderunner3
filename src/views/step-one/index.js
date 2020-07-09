cf7f7edfc3b15a2c204f96f9e390a973c5409a8f
Tyler Barabas <tylerbarabas@gmail.com>
2020-07-09 00:52:42 -0500

    allow input field to update qr code state

--- src/views/step-one/index.js
+++ src/views/step-one/index.js
@@ -1,30 +1,42 @@
-import React from 'react'
+import React, {
+  useContext,
+} from 'react'
+import { QrCodeContext } from '../../contexts/qrcode'
 
-class StepOne extends React.Component {
-  render(){
-    return (
-      <div className="columns is-fullheight is-vcentered is-hidden-mobile">
-        <div className="column">
-          <p className="is-size-2">4 steps to animate your code!</p>
-          <p className="is-size-2">Set your scan destination?</p>
-          <input
-            type="text"
-            placeholder="www.example.com"
-            className="input is-inline"
-            style={{
-              height: '50px',
-              width: '300px',
-              fontSize: '20px',
-            }}
-          />
-          <p
-            className="is-size-4"
-            style={{color: '#c2c4ff'}}
-          >Website? Social? Coupon?</p>
-        </div>
+const StepOne = () => {
+  const {
+    qrCode,
+    qrCodeMethods,
+  } = useContext(QrCodeContext)
+  const { str } = qrCode
+  const { updateStr } = qrCodeMethods
+  return (
+    <div className="columns is-fullheight is-vcentered is-hidden-mobile">
+      <div className="column">
+        <p className="is-size-2">4 steps to animate your code!</p>
+        <p className="is-size-2">Set your scan destination?</p>
+        <input
+          type="text"
+          placeholder="www.example.com"
+          className="input is-inline"
+          style={{
+            height: '50px',
+            width: '300px',
+            fontSize: '20px',
+          }}
+          onChange={e=>{
+            const { value } = e.target
+            updateStr(value)
+          }}
+          defaultValue={str}
+        />
+        <p
+          className="is-size-4"
+          style={{color: '#c2c4ff'}}
+        >Website? Social? Coupon?</p>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default StepOne
