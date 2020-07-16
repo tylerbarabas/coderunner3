9c661a25184238b35f7648bac0624889d3e4e30c
Tyler Barabas <tylerbarabas@gmail.com>
2020-07-15 23:42:33 -0500

    convert wrapper to functional component

--- src/views/wrapper/index.js
+++ src/views/wrapper/index.js
@@ -2,24 +2,19 @@ import React from 'react'
 import Preview from '../../components/preview'
 import StepOne from '../../views/step-one'
 
-class Wrapper extends React.Component {
-  render() {
-    const getPreviewUri = id => `https://api.acme.codes/orders/${id}/frames/1`
-    return (
-      <div className="section is-fullheight">
-        <div className="columns is-fullheight">
-          <div className="column is-half has-background-white">
-            <Preview
-              src={getPreviewUri('1593144341_QZB842DI')}
-            />
-          </div>
-          <div className="column is-half has-text-white has-text-centered">
-            <StepOne />
-          </div>
+const Wrapper = () => {
+  return (
+    <div className="section is-fullheight">
+      <div className="columns is-fullheight">
+        <div className="column is-half has-background-white">
+          <Preview />
+        </div>
+        <div className="column is-half has-text-white has-text-centered">
+          <StepOne />
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Wrapper
