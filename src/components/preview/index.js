53b16d46476b66a6b5cdbd6772579ae94c1645bc
Tyler Barabas <tylerbarabas@gmail.com>
2020-07-16 19:00:52 -0500

    continue with preview component

--- src/components/preview/index.js
+++ src/components/preview/index.js
@@ -2,11 +2,16 @@ import React, {
   useContext,
 } from 'react'
 import { QrCodeContext } from  '../../contexts/qrcode'
+import Service from '../../service'
 
+const { DOMAIN } = Service
+const _getFirstFrame = id => `${DOMAIN}/orders/${id}/frames/1`
+const _getMp4 = id => `${DOMAIN}/orders/${id}/mp4`
 const _getPreview = qrCode => {
   const {
     id,
     animation,
+    step,
   } = qrCode
   if (step < 3) {
     return (
@@ -19,7 +24,7 @@ const _getPreview = qrCode => {
           className="image is-1by1"
         >
           <img
-            src={src}
+            src={_getFirstFrame(id)}
           />
         </figure>
       </div>
@@ -27,7 +32,7 @@ const _getPreview = qrCode => {
   }
   return (
     <video
-      src={src}
+      src={_getMp4(id)}
       autoPlay
       playsInline
       loop
