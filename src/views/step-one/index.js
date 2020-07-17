3069a51b35594ffac4394a7c95bb94aa9ddc222c
Tyler Barabas <tylerbarabas@gmail.com>
2020-07-17 00:45:58 -0500

    responsive step one

--- src/views/step-one/index.js
+++ src/views/step-one/index.js
@@ -12,25 +12,33 @@ const StepOne = () => {
   const {
     responsive,
   } = useContext(ResponsiveContext)
+  const {
+    isMobile,
+  } = responsive
   const { str } = qrCode
   const { updateStr } = qrCodeMethods
+  const font = {
+    large: (isMobile) ? 'is-size-4' : 'is-size-2',
+    small: (isMobile) ? 'is-size-5' : 'is-size-4',
+  }
   return (
     <div className="columns is-fullheight is-vcentered">
       <div className="column">
-        <p className="is-size-2">4 steps to animate your code!</p>
-        <p className="is-size-2">Set your scan destination?</p>
+        <p className={font.large}>4 steps to animate your code!</p>
+        <p className={`${font.large} mb-6`}>Set your scan destination?</p>
         <input
           type="text"
           placeholder="www.example.com"
-          className="input is-inline"
+          className={`input ${font.small}`}
           onChange={e=>{
             const { value } = e.target
             updateStr(value)
           }}
           defaultValue={str}
+          style={{maxWidth: '300px'}}
         />
         <p
-          className="is-size-4"
+          className={`${font.small} mt-6`}
           style={{color: '#c2c4ff'}}
         >Website? Social? Coupon?</p>
       </div>
