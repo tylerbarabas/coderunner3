1c45fc2171fb2e7b50424294aadeb7ed2fcd55de
Tyler Barabas <tylerbarabas@gmail.com>
2020-07-19 21:41:33 -0500

    use orientation to assign max height

--- src/views/wrapper/index.js
+++ src/views/wrapper/index.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Preview from '../../views/preview'
 import StepOne from '../../views/step-one'
+import { ResponsiveContext } from '../../contexts/responsive'
 
 const Wrapper = () => {
+  const { responsive } = useContext(ResponsiveContext)
+  const maxHeight = (responsive.orientation === 'landscape') ? '100%' : '70%'
   return (
     <div className="section" style={{height: '95%'}}>
       <div className="columns is-fullheight">
-        <div className="column is-half has-background-white">
+        <div className="column is-half has-background-white" style={{maxHeight}}>
           <Preview />
         </div>
         <div className="column is-half has-text-white has-text-centered">
