2d41f7b61b8e6b825536c8060a9645aee32ba0b8
Tyler Barabas <tylerbarabas@gmail.com>
2020-07-22 15:03:22 -0500

    separate portrait and landscape layouts, and use flexbox instead of bulma  cols

--- src/views/wrapper/index.js
+++ src/views/wrapper/index.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Preview from '../../views/preview'
 import StepOne from '../../views/step-one'
+import { ResponsiveContext } from '../../contexts/responsive'
+import './index.scss'
 
 const Wrapper = () => {
+  const { responsive } = useContext(ResponsiveContext)
+  const { orientation } = responsive
+  const hidden = {
+    landscape: (orientation === 'landscape')?'':'is-hidden',
+    portrait: (orientation === 'portrait')?'':'is-hidden',
+  }  
   return (
-    <div className="level">
-      <div className="level-item has-background-white">
-        <Preview />
+    <div>
+      <div className={`row-container ${hidden.landscape}`}>
+        <div className="has-background-white">
+          <Preview />
+        </div>
+        <div className="">
+          <StepOne />
+        </div>
       </div>
-      <div className="level-item has-text-white has-text-centered">
-        <StepOne />
+      <div className={`col-container ${hidden.portrait}`}>
+        <div className="">
+          <Preview />
+        </div>
+        <div className="">
+          <StepOne />
+        </div>
       </div>
     </div>
   )
